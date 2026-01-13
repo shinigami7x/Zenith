cca31856f4b26bb5b3a6c14a131b93c86852b465
Shinigami <tsukiwindows2@gmail.com>
2026-01-13 16:01:49 -0500

    Remove winget script

--- optional/testing/zenith_driver.bat
+++ optional/testing/zenith_driver.bat
@@ -6,19 +6,12 @@
 
 rem Declare relevant paths
 set "DOCUMENTS=%USERPROFILE%\Documents"
-set "WINGET_SCRIPT=%DOCUMENTS%\Zenith\winget_bootstrap.ps1"
 set "APP_SCRIPT=%DOCUMENTS%\Zenith\app_installs.ps1"
 set "YASB_SCRIPT=%DOCUMENTS%\Zenith\yasb_bootstrap.bat"
 
 rem Verify that the files exists
-if not exist "%WINGET_SCRIPT%" goto :error
 if not exist "%APP_SCRIPT%" goto :error
 if not exist "%YASB_SCRIPT%" goto :error
-
-echo Launching winget bootsrap script...
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%WINGET_SCRIPT%"
-if errorlevel 1 goto :error
-echo Winget bootstrap script finished running.
 
 echo Launching app install script...
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%APP_SCRIPT%"
