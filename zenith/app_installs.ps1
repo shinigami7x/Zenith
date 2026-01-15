531d95bfbd365fc10611f2111ef254cfa137d566
Shinigami <tsukiwindows2@gmail.com>
2026-01-15 01:43:26 -0500

    Update app_installs.ps1

--- zenith/app_installs.ps1
+++ zenith/app_installs.ps1
@@ -23,8 +23,6 @@ Write-Host "Required app installs starting..."
 & $Winget install --id LGUG2Z.whkd -e --accept-source-agreements --accept-package-agreements
 & $Winget install --id AmN.yasb -e --accept-source-agreements --accept-package-agreements
 & $Winget install --id Zen-Team.Zen-Browser -e --accept-source-agreements --accept-package-agreements
-& $Winget install --id Discord.Discord -e --accept-source-agreements --accept-package-agreements
-# & $Winget install --id Spotify.Spotify -e --accept-source-agreements --accept-package-agreements
 & $Winget install --id Microsoft.VisualStudioCode -e --accept-source-agreements --accept-package-agreements
 
 Write-Host "Required app installs complete."
